3e6f67efffc464ee52d07f4962b9950e7d083b2f
pat <pat@localhost.localdomain>
2017-02-03 20:56:02 -0500

    bare bones module files

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pmclain_Stripe extension
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category  Pmclain
+ * @package   Pmclain_Stripe
+ * @copyright Copyright (c) 2016
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+  \Magento\Framework\Component\ComponentRegistrar::MODULE,
+  'Pmclain_Stripe',
+  __DIR__
+);
